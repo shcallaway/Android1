9f095e122185ff25aac2b6d171c5a6efbb73b9fa
Sherwood Callaway <shcallaway@gmail.com>
2016-11-15 15:42:03 -0500

    Implemented log activity with list of logs

--- app/src/main/java/com/shcallaway/myfirstapp/LogActivity.java
+++ app/src/main/java/com/shcallaway/myfirstapp/LogActivity.java
@@ -7,25 +7,61 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 public class LogActivity extends AppCompatActivity {
 
-    ArrayList<String> listValues = new ArrayList<>();
+    ArrayList<String> logArray = new ArrayList<>();
     ListView listView;
+    String logs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
 
-        // Get String version of logs from Intent
-        // Convert String version back to ArraySet
-        // Use SimpleDateFormat to format Strings as readable Dates
-        // Get pretty Dates as Strings; add them to listValues
+        // Retrieve logs from the Bundle.
+        Bundle bundle = getIntent().getExtras();
+        logs = bundle.getString("logs");
+
+        populateLogArrayFromLogs();
 
         listView = (ListView) findViewById(R.id.list);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, listValues);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, logArray);
         listView.setAdapter(adapter);
     }
+
+    private void populateLogArrayFromLogs() {
+        StringBuilder logBuilder = new StringBuilder("");
+
+        for (char ch : logs.toCharArray()) {
+            if (ch == ',') {
+                String unformattedLog = logBuilder.toString();
+                String formattedLog = formatLog(unformattedLog);
+                logArray.add(formattedLog);
+                logBuilder = new StringBuilder("");
+            } else {
+                logBuilder.append(ch);
+            }
+        }
+    }
+
+    private String formatLog(String unformattedLog) {
+        String formattedLog;
+
+        Date logDate = new Date(Long.parseLong(unformattedLog));
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z", Locale.US);
+        formattedLog = sdf.format(logDate);
+
+        // Do some magic to format the unformatted String...
+        return formattedLog;
+    }
 }
+
+// Make sure you can change timezone when reloading list activity, times change
+// wrap logging in logic: whether or not instance state bundle was changed (if null, app was just started)
--- app/src/main/java/com/shcallaway/myfirstapp/MainActivity.java
+++ app/src/main/java/com/shcallaway/myfirstapp/MainActivity.java
@@ -4,19 +4,16 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.ArraySet;
 import android.view.View;
 
 import java.util.Date;
-import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Declare constant for SharedPreferences file name.
     public static final String PREFS_NAME = "MyPrefsFile";
-
-    // Declare a Set for storing app usage logs.
-    Set<String> logs;
+    String logs;
+    SharedPreferences settings;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,41 +21,51 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Initialize SharedPreferences.
-        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        settings = getSharedPreferences(PREFS_NAME, 0);
 
-        // Check if logs StringSet already exists in SharedPreferences.
-        boolean logsDoesNotExist = (settings.getStringSet("logs", null) == null);
+        // Populate logs from SharedPreferences.
+        populateLogsFromPreferences();
 
-        if (logsDoesNotExist) {
-            // Initialize logs with an empty ArraySet.
-            logs = new ArraySet<>();
-        } else {
-            // Or, point logs to existing StringSet in SharedPreferences.
-            logs = settings.getStringSet("logs", null);
-        }
+        // Add a new log to logs for this instance.
+        addNewLogToLogs();
 
-        // Create a new log String from the current Date.
-        Date date = new Date();
-        String log = String.valueOf(date.getTime());
-
-        // Add the new log to logs.
-        logs.add(log);
-
-        // Create a SharedPreferences Editor.
-        SharedPreferences.Editor editor = settings.edit();
-
-        // Update the logs in SharedPreferences to include the new log.
-        editor.putStringSet("logs", logs);
-        editor.commit();
+        // Update set in SharedPreferences to match.
+        updateLogsInPreferences();
     }
 
-    public void logActivity(View view) {
-
-        // Intent might not be able to carry ArraySet (find another way)
-        // Maybe -- a bundle?
+    public void goToLogActivity(View view) {
 
         Intent intent = new Intent(this, LogActivity.class);
+        intent.putExtra("logs", logs);
         startActivity(intent);
     }
 
+    private void populateLogsFromPreferences() {
+        // Get logs from SharedPreferences.
+        String logsFromPreferences = settings.getString("logs", null);
+
+        // If logs does not exist, populate logs using logsFromPreferences.
+        if (logsFromPreferences != null) {
+            logs = logsFromPreferences;
+        }
+    }
+
+    private void updateLogsInPreferences() {
+        // Initialize the Editor.
+        editor = settings.edit();
+
+        // Replace old String in SharedPreferences with new one.
+        editor.putString("logs", logs);
+        editor.commit();
+    }
+
+    private String getCurrentDateString() {
+        // Return a log String of the current Date.
+        return String.valueOf(new Date().getTime());
+    }
+
+    private void addNewLogToLogs() {
+        // Concatenate new log to logs String.
+        logs += getCurrentDateString() + ",";
+    }
 }
